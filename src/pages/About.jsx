31a8862ee12b81bd8eb2907cb612da45540d2167
robert koopedi <koopedi05@gmail.com>
2025-05-04 22:05:14 +0200

    updated page components

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <main>
+        <h1>About VanLife</h1>
+        <p>We offer the best vans for your adventures.</p>
+      </main>
+    )
+  }
+  
