f0f903ee89ba244c93e28f06bfb2aa42fef49d9a
robert koopedi <koopedi05@gmail.com>
2025-05-05 01:49:50 +0200

    updated react.strictMode

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar.jsx"
 
 function App() {
 
